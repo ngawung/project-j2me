842c4e3b6b0f86d981d3b65ae9051d1a22c1f399
Ferdian <ferdian.artwork@gmail.com>
2021-05-04 04:33:37 +0000

    fix rendering outbound

--- src/com/melody/display/Quad.java
+++ src/com/melody/display/Quad.java
@@ -43,21 +43,19 @@ public class Quad extends Mobject {
 	public void render(Graphics g) {
 		if (visible) {
 			
-			// if object inside camera
-			if (CoordUtils.rectInRect((int)x + pivotX, (int)y + pivotY, (int)x+width + pivotX, (int)y+height + pivotY, (int)MainEngine.get_instance().get_scene().cameraX, (int)MainEngine.get_instance().get_scene().cameraY, (int)MainEngine.get_instance().get_scene().cameraX + MainEngine.get_instance().get_scene().get_width(), (int)MainEngine.get_instance().get_scene().cameraY + MainEngine.get_instance().get_scene().get_height())) {
-				
-				g.setColor(color);
-				if (followCamera) {
+			g.setColor(color);
+			if (followCamera) {
+				// if object inside camera
+				if (CoordUtils.rectInRect((int)x + pivotX, (int)y + pivotY, (int)x+width + pivotX, (int)y+height + pivotY, (int)MainEngine.get_instance().get_scene().cameraX, (int)MainEngine.get_instance().get_scene().cameraY, (int)MainEngine.get_instance().get_scene().cameraX + MainEngine.get_instance().get_scene().get_width(), (int)MainEngine.get_instance().get_scene().cameraY + MainEngine.get_instance().get_scene().get_height())) {
 					if (fill) g.fillRect((int)x - (int)MainEngine.get_instance().get_scene().cameraX + pivotX, (int)y - (int)MainEngine.get_instance().get_scene().cameraY + pivotY, width, height);
 					else g.drawRect((int)x - 1 - (int)MainEngine.get_instance().get_scene().cameraX + pivotX, (int)y - 1 - (int)MainEngine.get_instance().get_scene().cameraY + pivotY, width, height);
-				} else {
-					if (fill) g.fillRect((int)x + pivotX, (int)y + pivotY, width, height);
-					else g.drawRect((int)x - 1 + pivotX, (int)y - 1 + pivotY, width, height);
 				}
 				
+			} else {
+				if (fill) g.fillRect((int)x + pivotX, (int)y + pivotY, width, height);
+				else g.drawRect((int)x - 1 + pivotX, (int)y - 1 + pivotY, width, height);
 			}
-			
-			
+				
 		}
 
 	}
